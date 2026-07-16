aec854b19fc77205b9cd27cc1fda48e4b50df197
ShreyabhiD200 <duttashreyabhi3859@gmail.com>
2026-07-16 20:35:37 +0530

    Time: 3 ms (71.76%), Space: 46.5 MB (65.32%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -5,14 +5,14 @@ class Solution {
         int n = s.length();
         char[] ch = s.toCharArray();
         while(left<right){
-            if((ch[left] == 'a' || ch[left] == 'e' || ch[left] == 'i' || ch[left] == 'o' || ch[left] == 'u' || ch[left] == 'A' || ch[left] == 'E' || ch[left] == 'I' || ch[left] == 'O' || ch[left] == 'U') && (ch[right] == 'a' || ch[right] == 'e' || ch[right] == 'i' || ch[right] == 'o' || ch[right] == 'u' || ch[right] == 'A' || ch[right] == 'E' || ch[right] == 'I' || ch[right] == 'O' || ch[right] == 'U')){
+            if(isVowel(ch[left]) & isVowel(ch[right])){
                 char temp = ch[left];
                 ch[left] = ch[right];
                 ch[right] = temp;
 
                 left++;
                 right--;
-            } else if((ch[left] == 'a' || ch[left] == 'e' || ch[left] == 'i' || ch[left] == 'o' || ch[left] == 'u' || ch[left] == 'A' || ch[left] == 'E' || ch[left] == 'I' || ch[left] == 'O' || ch[left] == 'U')){
+            } else if(isVowel(ch[left])){
                 right--;
             } else{
                 left++;
@@ -20,5 +20,12 @@ class Solution {
         }
         String str = String.valueOf(ch);
         return str;
+    }
+
+    private boolean isVowel(char c){
+        return c == 'a' || c == 'e' || c == 'i' ||
+            c == 'o' || c == 'u' ||
+            c == 'A' || c == 'E' || c == 'I' ||
+            c == 'O' || c == 'U'; 
     }
 }
