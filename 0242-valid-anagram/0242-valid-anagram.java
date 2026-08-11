c3a9d48b554599dd409756dba3830527d4779ced
ShreyabhiD200 <duttashreyabhi3859@gmail.com>
2026-08-11 15:44:37 +0530

    Time: 19 ms (10.44%), Space: 47 MB (5.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -11,18 +11,15 @@ class Solution {
             }
 
             int n = b-1;
-            int last_index_of_j = 0;
-            int last_index_of_t = b-1;
             for(int j=0;j<b;j++){
                 if(map1.containsKey(t.charAt(j))){
                     map1.put(t.charAt(j), map1.get(t.charAt(j))-1);
                     if(map1.get(t.charAt(j))==0){
                         map1.remove(t.charAt(j));
                     }
-                    last_index_of_j = j;
                 }
             }
-            if(map1.isEmpty() && (last_index_of_j==last_index_of_t)){
+            if(map1.isEmpty()){
                 return true;
             }
             return false;
